6d59581d2e72ad0e24d1f85521cd2b5e4f573bf2
PSamu23 <124213006+PSamu23@users.noreply.github.com>
2024-04-05 20:40:46 -0600

    Add files via upload
    a

---
+++ proyecto bases 1/java/logg.js
@@ -0,0 +1,8 @@
+document.getElementById('crear-cuenta').onclick = function() {
+    document.getElementById('miModal').style.display = "block";
+  }
+  
+  document.getElementById('cerrarModal').onclick = function() {
+    document.getElementById('miModal').style.display = "none";
+  }
+  
